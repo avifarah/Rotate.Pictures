7272443047a83df0ecd67f7c8b78b00c7b24312a
Avi Farah <avi_farah@hotmail.com>
2021-02-26 12:16:57 -0500

    Added TODO to correct the algorithm to handle the do-not-display pictures

--- Rotate.Pictures/Utility/DoNotDisplayUtil.cs
+++ Rotate.Pictures/Utility/DoNotDisplayUtil.cs
@@ -12,6 +12,13 @@ namespace Rotate.Pictures.Utility
 
         enum DoNotDisplayAction { AddPicture, SaveAll };
 
+        /// <summary>
+        /// TODO: when we are reading the pictures do not save the pictures to file or configuration
+        /// TODO: when we are done reading then save the do-not-display to both file and configuration
+        /// </summary>
+        /// <param name="noDisplayItems"></param>
+        /// <param name="repositoryFilePath"></param>
+        /// <returns></returns>
         public static bool AddDoNotDisplay(IEnumerable<string> noDisplayItems, string repositoryFilePath = null)
 	        => UpdateDoNotDisplay(noDisplayItems, repositoryFilePath, DoNotDisplayAction.AddPicture);
 
